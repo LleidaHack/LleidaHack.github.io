899acf5c1c6c851c2b4199aefebc7df97871db4f
Guillem Orellana Trullols <guillem.orellana@gmail.com>
2019-03-10 22:32:07 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,10 +6,8 @@ $(document).ready(function() {
   $("#about-us").load("../home/about-us.html"); 
 
   // Load and append members
-  $("#members").load("../home/members.html"); 
-  appendMembers();
+  $("#members").load("../home/members.html", appendMembers); 
 
   // Load and append events
-  $("#events").load("../home/events.html"); 
-  appendEvents();
+  $("#events").load("../home/events.html", appendEvents); 
 });
