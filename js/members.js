035bbc4b8ee5fdd91d5b8f3778c59b95dc7614db
JordiROP <jordirop@gmail.com>
2019-06-20 23:27:54 +0200

    Corrected profile pictures

--- js/members.js
+++ js/members.js
@@ -7,7 +7,7 @@ function memberToHtml(member) {
   const role = member.status || "";
 
   return `
-    <div class="p-4 col-lg-3 col-md-3 col-sm-6">
+    <div class="p-4 col-lg-3 col-md-3 col-sm-6 col-6">
     <div class="team-member">
       <figure>
         <img src="${member.image}" width="20" class="img-responsive">
@@ -23,13 +23,21 @@ function memberToHtml(member) {
  * and append them to the document
  */
 function appendMembers() {
+  var count = 0;
+  var rowCount=0;
   $.getJSON("../data/members.json", function(members) {
-    // Convert lords and simple members to HTML
-    const membersHtml = members.members.map(memberToHtml);
+    // Convert members to HTML
+    const membersHtml = members.members.map(memberToHtml); 
 
     // Append members to document
-    membersHtml.forEach(element => {
-      $(".members-container").append(element);
+    for(i=1; i<membersHtml.length/4; i++) {
+      var rowMemberContainer = $.parseHTML(`<div class="members-container-${i} row justify-content-md-center"></div>`);
+      $("#pre-events-container").append(rowMemberContainer);
+    }
+    membersHtml.forEach(function(element, index) {
+      var num = Math.ceil((index+1)/4);
+      var membersContainer = ('.members-container-'.concat(num));
+      $(membersContainer).append(element);
     });
   });
 }
